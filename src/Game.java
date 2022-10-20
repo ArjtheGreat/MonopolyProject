0d719d84e72838c5f6f47aa61d9040c126d15730
Duke410 <dmduke4@gmail.com>
2022-10-20 09:30:25 -0500

    Board printing

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,6 @@
 /*********************
  *
- * Author: Arjun Maitra and Elliot Duke
+ * Author: Arjun Maitra and Elliott Duke
  * Assignment: Monopoly, Game Class
  * Date due: 10/26
  */
@@ -16,13 +16,15 @@ public class Game {
     public CircularLinkedList<Player> getPlayers() {
         return players;
     }
-
     public void setPlayers(CircularLinkedList<Player> players) {
         this.players = players;
     }
 
     public CircularLinkedList<BoardSpace> getGameBoard() {
         return gameBoard;
+    }
+    public void setGameBoard(CircularLinkedList<BoardSpace> gameBoard) {
+        this.gameBoard = gameBoard;
     }
 
     public BoardSpace getBoardSpace(int x) {
@@ -34,7 +36,50 @@ public class Game {
         return (BoardSpace) y.t;
     }
 
-    public void setGameBoard(CircularLinkedList<BoardSpace> gameBoard) {
-        this.gameBoard = gameBoard;
+    public void printBoard(){
+        //initialize dividers and spaces
+        char side = '|';
+        String blankSpace = "                                            ";
+        String halfBlank = "                  ";
+        String capDivider = "--------------------------------------------------------";
+        String lineDivider = "|----|";
+
+        //initialize height and width of window and lineStr
+        int height = 11;
+        int width = 11;
+        BoardSpace current = (BoardSpace) gameBoard.first;
+        //String currentSpace = ;
+        String lineSpace = "| "+currentSpace+" ";
+        //for each row in the window, print what should be on that row in a battle, depending on what row its on
+        System.out.println(capDivider);
+        for (int r = 0; r < height; r++) {
+            if (r == 0) {
+                for(int c = 0;c<width;c++){
+                    System.out.print(lineSpace);
+                }
+                System.out.println('|');
+                System.out.println(capDivider);
+            }
+            else if (r == 5) {
+                System.out.println(lineSpace+side+halfBlank+"MONOPOLY"+halfBlank+lineSpace+side);
+                System.out.println(lineDivider + blankSpace + lineDivider);
+            }
+            else if (r == 10) {
+                System.out.println(capDivider);
+                for(int c = 0;c<width;c++){
+                    System.out.print(lineSpace);
+                }
+                System.out.println('|');
+            }
+            else {
+                System.out.println(lineSpace+side+blankSpace+lineSpace+side);
+                System.out.println(lineDivider + blankSpace + lineDivider);
+            }
+        }
+        currentSpace = currentSpace.nextLink;
+
+        //print the bottom divider
+        System.out.println(capDivider);
+
     }
 }
